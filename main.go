7a4ccaab910e5b32baacee03295bf84a1816ea46
Jean-Pierre Bouchard <jeanpierre.bouchard80@gmail.com>
2017-12-17 20:07:52 -0500

    default Makefile with help

---
+++ main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"log"
+
+	"github.com/dgx80/goio"
+	"github.com/fatih/color"
+)
+
+var path = "./Makefile"
+
+func main() {
+
+	if goio.FileExist(path) {
+		log.Println("file exist")
+		failed("file exist")
+	}
+
+	if !goio.CreateFile(path) {
+		failed("unable to create file")
+	}
+	writeDefaultsFile()
+}
+
+func failed(message string) {
+	color.Red(message)
+	log.Fatalln("teminate with an error")
+}
+
+func writeDefaultsFile() {
+	var lines = []string{
+		".PHONY: help\n\n",
+		"help: ## help - Display callable targets. Hint: you can write just make to see help\n\t",
+		`@grep -E '(^[a-zA-Z_-]+:.*?##.*$$)|(^##)' $(MAKEFILE_LIST) | awk 'BEGIN {FS = ":.*?## "}; {printf "\033[32m%-20s\033[0m %s\n", $$1, $$2}' | sed -e 's/\[32m##/[33m/'`,
+		"\n",
+	}
+
+	for _, line := range lines {
+		goio.WriteFile(path, line)
+	}
+}
